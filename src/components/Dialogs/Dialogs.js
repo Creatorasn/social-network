4e3f39e2b8c920b73e03a9183da414d22a4d0ee4
Alexandr Nastychenko <asn14072020@gmail.com>
2021-10-18 09:51:25 +0600

    Create arrays for data inside Dialogs & MyPosts

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -13,20 +13,30 @@ const Message = (props) => {
     return <div className={s.message}>{props.message}</div>
 }
 
+let dialogsData = [
+    {id: 1, name: 'Squirrel'},
+    {id: 2, name: 'Fox'},
+    {id: 3, name: 'Tit'},
+    {id: 4, name: 'Wolf'},
+    {id: 5, name: 'Phoenix'}
+]
+
+let messagesData = [
+    {id: 1, message: 'When did the first scouts appear?'},
+    {id: 2, message: 'The scout movement originated a long time ago and still exists.'},
+    {id: 3, message: 'It was his basic organizational principles that were adopted by the pioneer organization.'}
+]
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <DialogItem name="Squirrel" id="1" />
-                <DialogItem name="Fox" id="2" />
-                <DialogItem name="Tit" id="3" />
-                <DialogItem name="Wolf" id="4" />
-                <DialogItem name="Phoenix" id="5" />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
             </div>
             <div className={s.messages}>
-                <Message message="When did the first scouts appear?" />
-                <Message message="The scout movement originated a long time ago and still exists." />
-                <Message message="It was his basic organizational principles that were adopted by the pioneer organization." />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,6 +1,12 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
+let postData = [
+    {id: 1, message: 'scout post Squirrel', likesCount: 0},
+    {id: 2, message: 'scout post Fox', likesCount: 10},
+    {id: 3, message: 'scout post Tit', likesCount: 20}
+]
+
 const MyPosts = () => {
     return (
         <div>
@@ -9,9 +15,9 @@ const MyPosts = () => {
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
-        <Post message='scout post Squirrel' likesCount='0' />
-            <Post message='scout post Fox' likesCount='10' />
-            <Post message='scout post Tit' likesCount='20' />
+            <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+            <Post message={postData[1].message} likesCount={postData[1].likesCount} />
+            <Post message={postData[2].message} likesCount={postData[2].likesCount} />
         </div>
     );
 }
