8495c9818b4566a5047b77820619a580c4ef7437
Alexandr Nastychenko <asn14072020@gmail.com>
2021-10-18 11:57:23 +0600

    Encapsulated components DialogItem & Message

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,36 +1,25 @@
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
-
-let dialogs = [
-    {id: 1, name: 'Squirrel'},
-    {id: 2, name: 'Fox'},
-    {id: 3, name: 'Tit'},
-    {id: 4, name: 'Wolf'},
-    {id: 5, name: 'Phoenix'}
-]
-
-let messages = [
-    {id: 1, message: 'When did the first scouts appear?'},
-    {id: 2, message: 'The scout movement originated a long time ago and still exists.'},
-    {id: 3, message: 'It was his basic organizational principles that were adopted by the pioneer organization.'}
-]
-
-let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} />);
-let messagesElements = messages.map( m => <Message message={m.message} />);
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
 const Dialogs = (props) => {
+    let dialogs = [
+        {id: 1, name: 'Squirrel'},
+        {id: 2, name: 'Fox'},
+        {id: 3, name: 'Tit'},
+        {id: 4, name: 'Wolf'},
+        {id: 5, name: 'Phoenix'}
+    ]
+    
+    let messages = [
+        {id: 1, message: 'When did the first scouts appear?'},
+        {id: 2, message: 'The scout movement originated a long time ago and still exists.'},
+        {id: 3, message: 'It was his basic organizational principles that were adopted by the pioneer organization.'}
+    ]
+    
+    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} />);
+    let messagesElements = messages.map( m => <Message message={m.message} />);
+    
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
