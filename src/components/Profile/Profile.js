e575e3548e6c9d56ddd871afd4cb2f21a72e4be8
Alexandr Nastychenko <asn14072020@gmail.com>
2021-10-18 15:46:09 +0600

    Transfer all data to index.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,17 +2,11 @@ import ProfileInfo from './ProfileInfo/ProfileInfo';
 import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
-let posts = [
-    {id: 1, message: 'scout post Squirrel', likesCount: 0},
-    {id: 2, message: 'scout post Fox', likesCount: 10},
-    {id: 3, message: 'scout post Tit', likesCount: 20}
-]
-
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={posts}/>
+            <MyPosts posts={props.posts}/>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+  {id: 1, message: 'scout post Squirrel', likesCount: 0},
+  {id: 2, message: 'scout post Fox', likesCount: 10},
+  {id: 3, message: 'scout post Tit', likesCount: 20}
+]
+
+let dialogs = [
+  {id: 1, name: 'Squirrel'},
+  {id: 2, name: 'Fox'},
+  {id: 3, name: 'Tit'},
+  {id: 4, name: 'Wolf'},
+  {id: 5, name: 'Phoenix'}
+]
+
+let messages = [
+  {id: 1, message: 'When did the first scouts appear?'},
+  {id: 2, message: 'The scout movement originated a long time ago and still exists.'},
+  {id: 3, message: 'It was his basic organizational principles that were adopted by the pioneer organization.'}
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>,
   document.getElementById('root')
 );
