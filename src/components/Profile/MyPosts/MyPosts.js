1fc84a21bc42de97bfd520bc681c87cdc36f2f54
Alexandr Nastychenko <asn14072020@gmail.com>
2021-10-21 09:36:59 +0600

    In textarea we make in dependence in UI on props
    * rewrite textarea into MyPosts
    * add function to onPostChange into MyPosts
    * add state.profilePage.newPostText into state.js
    * add function updateNewPostText into state.js
    * throw newPostText & updateNewPostText across props
    * fix the function newPost into MyPosts
    * fix the function addPost into state.js

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -8,10 +8,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
     
-    let addPost = () => {
+    let newPost = () => {
+        props.addPost();
+    }
+
+    let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.addPost(text);
-        newPostElement.current.value = '';
+        props.updateNewPostText(text);
     }
 
     return (
@@ -19,10 +22,10 @@ const MyPosts = (props) => {
             <h3>My post</h3>
             <div className={s.addPost}>
                 <div>
-                    <textarea ref={newPostElement}></textarea>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
                 </div>
                 <div>
-                    <button onClick={ addPost }>Add post</button>
+                    <button onClick={ newPost }>Add post</button>
                 </div>
             </div>
             <div>
--- src/redux/state.js
+++ src/redux/state.js
@@ -6,7 +6,8 @@ let state = {
             {id: 1, message: 'scout post Squirrel', likesCount: 0},
             {id: 2, message: 'scout post Fox', likesCount: 10},
             {id: 3, message: 'scout post Tit', likesCount: 20}
-        ]
+        ],
+        newPostText: 'scouting'
     },
 
     dialogsPage: {
@@ -26,14 +27,20 @@ let state = {
     }
   }
 
-export let addPost = (postMassage) => {
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMassage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     };
 
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
