9e59fe550e82edf9e99b74da32c9d4a362a828c4
Alexandr Nastychenko <asn14072020@gmail.com>
2021-11-09 13:19:38 +0600

    StateCopy into reducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -19,14 +19,18 @@ let initialState = {
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_MESSAGE:
+        case ADD_MESSAGE: {
             let newMessage = { message: state.newMessageText };
-            state.messages.push(newMessage);
-            state.newMessageText = '';
-            return state;
-        case UPDATE_NEW_MESSAGE_TEXT:
-            state.newMessageText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.messages.push(newMessage);
+            stateCopy.newMessageText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_MESSAGE_TEXT: {
+            let stateCopy = {...state};
+            stateCopy.newMessageText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
