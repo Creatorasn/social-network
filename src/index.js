dcb51a1440ddf03f4f7c227a87d13dd076a4afd9
Alexandr Nastychenko <asn14072020@gmail.com>
2021-11-09 15:08:23 +0600

    Rifactoring reducers, added Key into map

--- src/index.js
+++ src/index.js
@@ -7,24 +7,18 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 
-export let rerenderEntireTree = () => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </BrowserRouter>
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-  }
 
-rerenderEntireTree();
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-store.subscribe( () =>{
-  rerenderEntireTree();
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
